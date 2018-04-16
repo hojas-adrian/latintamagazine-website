70e0c5263240aa55a22961c7995ba30f7046c046
Geidel Guerra <geidelguerra@gmail.com>
2018-04-16 15:22:25 -0400

    Make download open in new window

--- pages/index.vue
+++ pages/index.vue
@@ -22,7 +22,7 @@
         </div>
 
         <div class="mt-4">
-          <a class="flex items-center" href="https://drive.google.com/file/d/1oVsYBnQvM5PaEeG5ukx633hkVQabg1d4/view?usp=sharing">
+          <a class="flex items-center" target="_blank" href="https://drive.google.com/file/d/1oVsYBnQvM5PaEeG5ukx633hkVQabg1d4/view?usp=sharing">
             <div class="flex-1">
               <div class="flex items-center flex-1">
                 <div class="flex-1 truncate">
@@ -42,7 +42,7 @@
             <svg class="fill-current text-black w-12" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32.3 32.46"><defs><style>.cls-2{clip-path:url(#clip-path)}</style><clipPath id="clip-path"><path fill="none" d="M0 0h32.3v32.47H0z"/></clipPath></defs><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path d="M32.3 32.47H0V0h32.3zm-30.3-2h28.3V2H2z"/><path d="M16.15 21.72l-7.16-6.84 1.39-1.45 5.77 5.52 5.77-5.52 1.39 1.45-7.16 6.84z"/><path d="M15.15.73h2v19.5h-2z"/></g></g></svg>
           </a>
 
-          <a class="flex items-center mt-4" href="https://drive.google.com/file/d/1g-UDphj1jfslpZX9kMqE34_xjjRrk04E/view?usp=sharing">
+          <a class="flex items-center mt-4" target="_blank" href="https://drive.google.com/file/d/1g-UDphj1jfslpZX9kMqE34_xjjRrk04E/view?usp=sharing">
             <div class="flex-1">
               <div class="flex items-center flex-1">
                 <div class="flex-1 truncate">
