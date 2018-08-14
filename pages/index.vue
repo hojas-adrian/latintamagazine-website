1c27ff64c5a47d6b302c215a2c917282553d2508
Geidel Guerra <geidelguerra@gmail.com>
2018-08-13 21:59:38 -0400

    Add new issue and fix some styling problems.

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template lang="html">
-  <div class="container mx-auto flex flex-col min-h-screen">
+  <div class="container mx-auto flex flex-col min-h-screen max-w-xl">
     <div class="p-4 flex">
       <div class="text-center flex flex-col items-center w-12">
         <img src="~/assets/img/logo.svg" alt="La Tinta Magazine" class="flex-none">
@@ -10,43 +10,44 @@
       </div>
     </div>
 
-    <div class="flex-1 flex justify-center flex-wrap">
-      <template v-for="(issue, i) in issues">
-        <div class="max-w-sm pl-4 pr-4 mb-4">
-          <div class="text-center pb-2 font-sans-1 font-medium">
-            {{ issue.title }}
-          </div>
+    <div class="flex flex-1 justify-center">
+      <div class="flex flex-wrap">
+        <template v-for="(issue, i) in issues">
+          <div class="w-full pl-4 pr-4 mb-4 md:w-1/2">
+            <div class="text-center pb-2 font-sans-1 font-medium">
+              {{ issue.title }}
+            </div>
 
-          <div>
-            <img :src="issue.cover" :alt="issue.title" class="max-w-full h-auto">
-          </div>
+            <div>
+              <img :src="issue.cover" :alt="issue.title" class="w-full h-auto">
+            </div>
 
-          <div class="mt-4">
-            <template v-for="(link, j) in issue.links">
-              <a class="flex items-center" target="_blank" :class="{'mt-4': j > 0}" :href="link.url">
-                <div class="flex-1">
-                  <div class="flex items-center flex-1">
-                    <div class="flex-1 truncate">
-                      <div class="uppercase font-sans-1 font-medium truncate">
-                        {{ link.text }}
+            <div class="mt-4">
+              <template v-for="(link, j) in issue.links">
+                <a class="flex items-center" target="_blank" :class="{'mt-4': j > 0}" :href="link.url">
+                  <div class="flex-1">
+                    <div class="flex items-center flex-1">
+                      <div class="flex-1 truncate">
+                        <div class="uppercase font-sans-1 font-medium truncate">
+                          {{ link.text }}
+                        </div>
+                      </div>
+
+                      <div class="pr-4">
+                        {{ link.size }}
                       </div>
                     </div>
-
-                    <div class="pr-4">
-                      {{ link.size }}
+                    <div class="text-sm font-sans-1 font-medium">
+                      {{ link.description }}
                     </div>
                   </div>
-                  <div class="text-sm font-sans-1 font-medium">
-                    {{ link.description }}
-                  </div>
-                </div>
-                <svg class="fill-current text-black w-12" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32.3 32.46"><defs><style>.cls-2{clip-path:url(#clip-path)}</style><clipPath id="clip-path"><path fill="none" d="M0 0h32.3v32.47H0z"/></clipPath></defs><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path d="M32.3 32.47H0V0h32.3zm-30.3-2h28.3V2H2z"/><path d="M16.15 21.72l-7.16-6.84 1.39-1.45 5.77 5.52 5.77-5.52 1.39 1.45-7.16 6.84z"/><path d="M15.15.73h2v19.5h-2z"/></g></g></svg>
-              </a>
-            </template>
+                  <svg class="fill-current text-black w-12" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32.3 32.46"><defs><style>.cls-2{clip-path:url(#clip-path)}</style><clipPath id="clip-path"><path fill="none" d="M0 0h32.3v32.47H0z"/></clipPath></defs><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path d="M32.3 32.47H0V0h32.3zm-30.3-2h28.3V2H2z"/><path d="M16.15 21.72l-7.16-6.84 1.39-1.45 5.77 5.52 5.77-5.52 1.39 1.45-7.16 6.84z"/><path d="M15.15.73h2v19.5h-2z"/></g></g></svg>
+                </a>
+              </template>
+            </div>
           </div>
-        </div>
-      </template>
-
+        </template>
+      </div>
     </div>
 
     <footer>
@@ -115,6 +116,24 @@ export default {
               url: 'http://bit.ly/LaTinta01_144dpi',
               description: '(144ppi)',
               size: '30.2Mb',
+            },
+          ]
+        },
+        {
+          title: 'No. 02 JULIO-AGOSTO',
+          cover: '/issues/covers/n02-cover.jpg',
+          links: [
+            {
+              text: 'Descargar #02',
+              url: 'http://bit.ly/LaTinta02_96dpi',
+              description: '(92ppi)',
+              size: '19.6Mb',
+            },
+            {
+              text: 'Descargar #02',
+              url: 'http://bit.ly/LaTinta02_144dpi',
+              description: '(144ppi)',
+              size: '34Mb',
             },
           ]
         },
